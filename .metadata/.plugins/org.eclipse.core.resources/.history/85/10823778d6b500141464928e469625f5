84bda5f1406dfe67f39805c5f8b75c1a0587175a
bttalic <b.ttalic@gmail.com>
2015-02-16 16:50:44 +0100

    mvc

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/85/10823778d6b500141464928e469625f5
@@ -0,0 +1,5 @@
+package ba.bitcamp.model;
+
+public class Application {
+
+}
---
+++ SQLiteIntro/src/ba/bitcamp/intro/DBConnection.java
@@ -0,0 +1,18 @@
+package ba.bitcamp.intro;
+
+import java.sql.*;
+
+public class DBConnection {
+
+	public static void main(String[] args) {
+		Connection c = null;
+		try {
+			c = DriverManager.getConnection("jdbc:sqlite:bitbase.db");
+			System.out.println("Connected");
+		} catch (SQLException e) {
+			System.err.println(e.getMessage());
+		}
+
+	}
+
+}
